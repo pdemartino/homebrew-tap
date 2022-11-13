4e071b67d5dc6d4aefdcbb1196f9961dd72f4aaf
Pasquale De Martino <paco.dmp@gmail.com>
2022-11-13 21:07:10 +0100

    Adds first release of springboot-startup-stats

--- Formula/springboot-startup-stats.rb
+++ Formula/springboot-startup-stats.rb
@@ -1,7 +1,8 @@
 class SpringbootStartupStats < Formula
   desc "Extracts Statistics from Spring Boot start-up actuator"
   homepage "https://github.com/pdemartino/springboot-startup-stats"
-  url "https://github.com/pdemartino/springboot-startup-stats/archive/refs/tags/v0.0.6.tar.gz"
+  url "https://github.com/pdemartino/springboot-startup-stats/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "7c990e99f6f0bb1f3d6c83deab5fb507a8811231af1d824a545e69a53653b990"
   license "GPL-3.0-or-later"
 
   depends_on "openjdk@17"
