614b39ec2b277f824ecdbef27aac9c3a04f556d4
Pasquale De Martino <paco.dmp@gmail.com>
2022-11-14 15:42:11 +0100

    Changes springboot-startup-stats hash

--- Formula/springboot-startup-stats.rb
+++ Formula/springboot-startup-stats.rb
@@ -2,7 +2,7 @@ class SpringbootStartupStats < Formula
   desc "Extracts Statistics from Spring Boot start-up actuator"
   homepage "https://github.com/pdemartino/springboot-startup-stats"
   url "https://github.com/pdemartino/springboot-startup-stats/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "7c990e99f6f0bb1f3d6c83deab5fb507a8811231af1d824a545e69a53653b990"
+  sha256 "f89d473e9669dde70922626dd9bad7a1dbe9f6dac8e07bd4cb39967fdffafdba"
   license "GPL-3.0-or-later"
 
   depends_on "openjdk@17"
