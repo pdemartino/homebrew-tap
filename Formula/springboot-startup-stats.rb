03720254defbdf61cce74c4078338857b2e0d3f1
Pasquale De Martino <paco.dmp@gmail.com>
2022-11-28 18:14:45 +0100

    Removes batch scripts

--- Formula/springboot-startup-stats.rb
+++ Formula/springboot-startup-stats.rb
@@ -4,11 +4,13 @@ class SpringbootStartupStats < Formula
   url "https://github.com/pdemartino/springboot-startup-stats/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "f89d473e9669dde70922626dd9bad7a1dbe9f6dac8e07bd4cb39967fdffafdba"
   license "GPL-3.0-or-later"
+  revision 1
 
   depends_on "openjdk@17"
 
   def install
     system "./gradlew", "-PinstallPrefix=#{prefix}", "installDist"
+    rm "#{prefix}/bin/*.bat"
   end
 
   test do
